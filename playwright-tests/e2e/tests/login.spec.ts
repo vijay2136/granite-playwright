1c78142fbefc2875f91171c6c0ecc9acb4d58ecd
vijay2136 <imvijay2136@gmail.com>
2023-12-21 22:30:59 +0530

    Added tests for Register page and created POMs and fixtures for Login

--- playwright-tests/e2e/tests/login.spec.ts
+++ playwright-tests/e2e/tests/login.spec.ts
@@ -1,15 +1,17 @@
-import { test, expect } from "@playwright/test";
+// login.spec.js
+
+import { test } from "../fixtures";
 
 test.describe("Login page", () => {
-  test("should login with the correct credentials", async ({ page }) => {
-    await page.goto("http://localhost:3000/login");
-    await page.getByTestId("login-email-field").fill("oliver@example.com");
-    await page.getByTestId("login-password-field").fill("welcome");
-    const btn = await page.getByTestId("login-submit-button");
-    await btn.click();
-    console.log(btn);
-    await expect(page.getByTestId("navbar-username-label")).toHaveText(
-      "Oliver Smith"
-    );
+  test("should login with the correct credentials", async ({
+    page,
+    loginPage,
+  }) => {
+    await page.goto("http://localhost:3000");
+    await loginPage.loginAndVerifyUser({
+      email: "oliver@example.com",
+      password: "welcome",
+      username: "Oliver Smith",
+    });
   });
 });
